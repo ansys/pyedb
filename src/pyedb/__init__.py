fb242dceb23222d379352ffc5a4764ae4115f0c5
Sebastien Morais <sebastien.morais@ansys.com>
2023-10-31 09:40:35 +0100

    BUGFIX: import Edb from pyedb

--- src/pyedb/__init__.py
+++ src/pyedb/__init__.py
@@ -9,3 +9,9 @@ pyedb_path = os.path.dirname(__file__)
 __version__ = "0.0.1"
 
 version = __version__
+
+# By default we use pyedb legacy implementation
+if "PYEDB_USE_LEGACY" not in os.environ:
+    os.environ["PYEDB_USE_LEGACY"] = "1"
+
+from pyedb.generic.design_types import Edb
