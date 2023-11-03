261645b5cc0ad6b942807eb090673c155b2a810b
Sebastien Morais <sebastien.morais@ansys.com>
2023-11-03 17:39:14 +0100

    MAINT: integrate pyaedt PR 3797 changes

--- tests/legacy/system/test_edb.py
+++ tests/legacy/system/test_edb.py
@@ -494,6 +494,8 @@ class TestClass:
         gap_port.name = "gap_port"
         assert gap_port.name == "gap_port"
         assert isinstance(gap_port.renormalize_z0, tuple)
+        gap_port.is_circuit_port = True
+        assert gap_port.is_circuit_port
         edb.close()
 
     def test_create_dc_simulation(self):
