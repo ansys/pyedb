8d1f97483b8e2ce974ad2daca5b179db2132fcd5
svandenb <simon.vandenbrouck@ansys.com>
2024-01-26 13:58:25 +0100

    edb config first draft

--- src/pyedb/configuration/data/component_data.py
+++ src/pyedb/configuration/data/component_data.py
@@ -3,9 +3,9 @@ from enum import Enum
 
 class PinpairModel:
     def __init__(self):
-        self._type = "series"
-        self._pin1 = ""
-        self._pin2 = ""
+        self.type = "series"
+        self.pin1 = ""
+        self.pin2 = ""
 
 
 class RlcModel:
@@ -24,7 +24,7 @@ class Type(Enum):
 
 class Component(Type):
     def __init__(self):
-        self._part_type = Type.RESISTOR
-        self._ref_des = ""
-        self._enabled = True
-        self._model = RlcModel()
+        self.part_type = Type.RESISTOR
+        self.ref_des = ""
+        self.enabled = True
+        self.model = RlcModel()
