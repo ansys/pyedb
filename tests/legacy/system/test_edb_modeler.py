31cd4d14c32ee7bf6dd1472e71931a94bc1088da
gkorompi <george.korompilis@ansys.com>
2024-03-07 15:07:52 +0200

    duplicate across layers new function

--- tests/legacy/system/test_edb_modeler.py
+++ tests/legacy/system/test_edb_modeler.py
@@ -349,15 +349,14 @@ class TestClass:
         edbapp.stackup.add_layer("top_gnd", thickness="0.65mil")
         edbapp.stackup.add_layer("d4", layer_type="dielectric", thickness="13mil", material="FR4_epoxy")
         edbapp.stackup.add_layer("trace1", thickness="$H")
-        r1 = edbapp.modeler.create_rectangle(center_point=("0,0"), width="200mil", height="200mil",layer_name="top_gnd", representation_type="CenterWidthHeight",net_name="r1")
-        r2 = edbapp.modeler.create_rectangle(center_point=("0,0"), width="40mil", height="$Antipad_R*2",layer_name="top_gnd", representation_type="CenterWidthHeight",net_name="r2")
-        c1 = edbapp.modeler.create_circle(layer_name="top_gnd", x="Via_S/2", y="0mil", radius="$Antipad_R",net_name="c1")
-        c2 = edbapp.modeler.create_circle(layer_name="top_gnd", x="-Via_S/2", y="0mil", radius="$Antipad_R",net_name="c2")
+        r1 = edbapp.modeler.create_rectangle(center_point=("0,0"), width="200mil", height="200mil", layer_name="top_gnd", representation_type="CenterWidthHeight", net_name="r1")
+        r2 = edbapp.modeler.create_rectangle(center_point=("0,0"), width="40mil", height="$Antipad_R*2", layer_name="top_gnd", representation_type="CenterWidthHeight", net_name="r2")
+        c1 = edbapp.modeler.create_circle(layer_name="top_gnd", x="Via_S/2", y="0mil", radius="$Antipad_R", net_name="c1")
+        c2 = edbapp.modeler.create_circle(layer_name="top_gnd", x="-Via_S/2", y="0mil", radius="$Antipad_R", net_name="c2")
         r = [r2, c1, c2]
         r1.subtract(r)
         lay_list = ["bot_gnd", "mid_gnd"]
-        edbapp.modeler.primitives[0].clone()
-        edbapp.modeler.primitives[0].duplcate_across_layers(lay_list)
-        edbapp.modeler.primitives[0]
+        edbapp.modeler.primitives_by_layer
+        edbapp.modeler.primitives[0].duplicate_across_layers(lay_list)
         edbapp.modeler.primitives_by_layer
         edbapp.close()
