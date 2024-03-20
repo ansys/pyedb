960835e8beb7dc9cb6f9b4869a982fe8f57fa873
Sebastien Morais <sebastien.morais@ansys.com>
2024-03-20 10:36:23 +0100

    DOC: Refact make.bat and Makefile

--- doc/make.bat
+++ doc/make.bat
@@ -12,9 +12,21 @@ if "%SPHINXOPTS%" == "" (
 )
 set SOURCEDIR=source
 set BUILDDIR=_build
+set LINKCHECKDIR=\%BUILDDIR%\linkcheck
+set LINKCHECKOPTS=-d %BUILDDIR%\.doctrees -W --keep-going --color
+
+REM This LOCs are used to uninstall and install specific package(s) during CI/CD
+for /f %%i in ('pip freeze ^| findstr /c:"vtk-osmesa"') do set is_vtk_osmesa_installed=%%i
+if NOT "%is_vtk_osmesa_installed%" == "vtk-osmesa" if "%ON_CI%" == "True" (
+	echo "Removing package(s) to avoid conflicts with package(s) needed for CI/CD"
+	pip uninstall --yes vtk
+	echo "Installing CI/CD required package(s)"
+	pip install --extra-index-url https://wheels.vtk.org vtk-osmesa==9.2.20230527.dev0)
+REM End of CICD dedicated setup
 
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
+if "%1" == "html" goto html
 if "%1" == "pdf" goto pdf
 
 %SPHINXBUILD% >NUL 2>NUL
@@ -30,12 +42,28 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
+
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
 :clean
-rmdir /s /q %BUILDDIR% > /NUL 2>&1
+echo Cleaning everything
+rmdir /s /q %SOURCEDIR%\examples > /NUL 2>&1 
+rmdir /s /q %BUILDDIR% > /NUL 2>&1 
 for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
+goto end
+
+:html:
+echo Building HTML pages with running examples
+::FIXME: currently linkcheck freezes and further investigation must be performed
+::%SPHINXBUILD% -M linkcheck %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %LINKCHECKOPTS% %O%
+%SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+echo
+echo "Build finished. The HTML pages are in %BUILDDIR%."
 goto end
 
 :pdf
@@ -48,9 +76,6 @@ if NOT EXIST ansys-edb.pdf (
 	exit /b 1)
 Echo "pdf generated!"
 goto end
-
-:help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
 :end
 popd
