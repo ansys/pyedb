5676a65b8698d33d69cbdd5d2b0e2320fa99241f
svandenb-dev <74993647+svandenb-dev@users.noreply.github.com>
2024-04-05 16:56:50 +0200

    Update src/pyedb/dotnet/edb_core/stackup.py
    Co-authored-by: Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>

--- src/pyedb/dotnet/edb_core/stackup.py
+++ src/pyedb/dotnet/edb_core/stackup.py
@@ -2193,8 +2193,8 @@ class Stackup(object):
         ----------
         file_path : str
             Path to stackup file.
-        mode : int
-            mode=`0` layer in layout not found in the stackup file are deleted and, mode=`1` if the number of layer
+        rename : bool
+            If rename is ``False`` then layer in layout not found in the stackup file are deleted. Otherwise, if the number of layer
             in the stackup file equals the number of stackup layer in the layout, layers are renamed according the the
             file. Note that layer order matters, and has to be writtent from top to bottom layer in the file.
 
