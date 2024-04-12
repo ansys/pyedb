fc6e7ec76188edaf07d3352bf4cdeaae280c4fdd
gkorompi <george.korompilis@ansys.com>
2024-04-12 09:11:37 +0300

    FEATURE: rlc_component multi pins

--- src/pyedb/dotnet/edb.py
+++ src/pyedb/dotnet/edb.py
@@ -3637,7 +3637,8 @@ class Edb(Database):
         >>> setup1.hfss_port_settings.max_delta_z0 = 0.5
         """
         if name in self.setups:
-            return self.logger.info("setup already exists")
+            self.logger.info("setup already exists")
+            return False
         setup = HfssSimulationSetup(self).create(name)
         return setup
 
