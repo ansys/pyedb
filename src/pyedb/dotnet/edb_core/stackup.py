67b55324150faf05a23ac14c1113cef9a1e138d4
svandenb-dev <simon.vandenbrouck@ansys.com>
2024-05-08 10:10:52 +0200

    stackup xml import unit issue fix

--- src/pyedb/dotnet/edb_core/stackup.py
+++ src/pyedb/dotnet/edb_core/stackup.py
@@ -2344,6 +2344,8 @@ class Stackup(LayerCollection):
                 }
                 for i in temp_dict.keys():
                     value = temp.get(i, None)
+                    if i == "Thickness":
+                        value = str(round(float(value), 6)) + length_unit
                     if value:
                         value = "signal" if value == "conductor" else value
                         if i == "Color":
