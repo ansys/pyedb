67f119251c7347bdd86f4cf9a30f5307df158736
ring630 <@gmail.com>
2024-06-04 21:59:34 +0200

    REACTOR: components, stackup

--- src/pyedb/configuration/cfg_stackup.py
+++ src/pyedb/configuration/cfg_stackup.py
@@ -19,8 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import json
-import os.path
 
 from pyedb.configuration.cfg_common import CfgBase
 from pyedb.generic.general_methods import pyedb_function_handler
