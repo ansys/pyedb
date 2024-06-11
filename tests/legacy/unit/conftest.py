8ead63fbf4386a0f0fb800d5537bf82f4efa83e2
adimaria <alberto.dimaria@ansys.com>
2024-06-11 14:40:38 +0200

    simplified fixture

--- tests/legacy/unit/conftest.py
+++ tests/legacy/unit/conftest.py
@@ -49,44 +49,4 @@ def points_for_line_detection():
     # START
     pts = np.array(points)
 
-    # Enlarge the point base
-    n = 2
-    m = 2
-
-    min_x = pts[:, 0].min()
-    max_x = pts[:, 0].max()
-    min_y = pts[:, 1].min()
-    max_y = pts[:, 1].max()
-    size_x = max_x - min_x
-    size_y = max_y - min_y
-
-    size_y_n = size_y * 1.5
-
-    # Function to rotate points
-    def rotate_points(points, angle_degrees):
-        angle_radians = np.radians(angle_degrees)
-        # Create the rotation matrix
-        rotation_matrix = np.array(
-            [[np.cos(angle_radians), -np.sin(angle_radians)], [np.sin(angle_radians), np.cos(angle_radians)]]
-        )
-        # Apply the rotation matrix to each point
-        rotated_points = points.dot(rotation_matrix.T)
-        return rotated_points
-
-    center = np.array([(min_x + max_x) / 2, (min_y + max_y) / 2])
-    centered_points = pts - center
-    # axis autoscale: squared
-    centered_points[:, 0] = centered_points[:, 0] / size_x * size_y
-    pts2 = centered_points
-    for r in range(n):
-        for c in range(m):
-            if r == 0 and c == 0:
-                continue
-            rotated_points = rotate_points(centered_points, 15 * (r + 1) * (c + 1))
-
-            move_vector = np.array([r * size_y_n, c * size_y_n])
-            additional_points = rotated_points + move_vector
-            # Add the additional points to the initial array
-            pts2 = np.vstack((pts2, additional_points))
-
-    return pts2
+    return pts
