2f7857dff1d3385ec3ae54d42603ae5804c9282f
Sebastien Morais <sebastien.morais@ansys.com>
2024-07-03 14:15:49 +0200

    DOCS: Refact standalone

--- examples/legacy_standalone/00_EDB_Create_VIA.py
+++ examples/legacy_standalone/00_EDB_Create_VIA.py
@@ -1,91 +1,51 @@
-# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
-# SPDX-License-Identifier: MIT
-#
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# # EDB: geometry creation
 
-"""
-EDB: geometry creation
-----------------------
-This example shows how you can use EDB to create a layout.
-"""
-######################################################################
-# Final expected project
-# ~~~~~~~~~~~~~~~~~~~~~~
+# This example shows how you can use EDB to create a layout.
+# ## Final expected project
 #
-# .. image:: ../../_static/diff_via.png
-#  :width: 600
-#  :alt: Differential Vias.
-######################################################################
-
-######################################################################
-# Import EDB layout object
-# ~~~~~~~~~~~~~~~~~~~~~~~~
+# <img src="_static/diff_via.png" width="500">
+#
+# ## Import EDB layout object
 # Import the EDB layout object and initialize it on version 2023 R2.
-######################################################################
 
+# +
 import os
-import time
-
+import tempfile
 import pyedb
-from pyedb.generic.general_methods import (
-    generate_unique_folder_name,
-    generate_unique_name,
-)
 
-start = time.time()
+temp_dir = tempfile.TemporaryDirectory(suffix=".ansys")
+aedb_path = os.path.join(temp_dir.name, "create_via.aedb")
+print(f"AEDB file path: {aedb_path}")
 
-aedb_path = os.path.join(generate_unique_folder_name(), generate_unique_name("pcb") + ".aedb")
-edb = pyedb.Edb()
-edb.save_edb_as(aedb_path)
+# Select EDB version (change it manually if needed, e.g. "2024.1")
+edb_version = "2024.1"
+print(f"EDB version: {edb_version}")
 
+edb = pyedb.Edb(edbpath=aedb_path, edbversion=edb_version)
+# -
 
-####################
-# Add stackup layers
-# ~~~~~~~~~~~~~~~~~~
+# ## Add stackup layers
 # Add stackup layers.
-# A stackup can be created layer by layer or imported from a csv file or xml file.
-#
+# A stackup can be created layer by layer or imported from a CSV file or XML file.
 
 edb.stackup.add_layer("GND")
-edb.stackup.add_layer("Diel", "GND", layer_type="dielectric", thickness="0.1mm", material="FR4_epoxy")
+edb.stackup.add_layer(
+    "Diel", "GND", layer_type="dielectric", thickness="0.1mm", material="FR4_epoxy"
+)
 edb.stackup.add_layer("TOP", "Diel", thickness="0.05mm")
 
-#####################################
-# Create signal net and ground planes
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ## Create signal net and ground planes
 # Create a signal net and ground planes.
 
-points = [
-    [0.0, 0],
-    [100e-3, 0.0],
-]
+points = [[0.0, 0], [100e-3, 0.0]]
 edb.modeler.create_trace(points, "TOP", width=1e-3)
 points = [[0.0, 1e-3], [0.0, 10e-3], [100e-3, 10e-3], [100e-3, 1e-3], [0.0, 1e-3]]
 edb.modeler.create_polygon(points, "TOP")
-
 points = [[0.0, -1e-3], [0.0, -10e-3], [100e-3, -10e-3], [100e-3, -1e-3], [0.0, -1e-3]]
 edb.modeler.create_polygon(points, "TOP")
 
-#######################################
-# Create vias with parametric positions
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ## Create vias with parametric positions
 # Create vias with parametric positions.
 
 edb.padstacks.create("MyVia")
@@ -99,27 +59,26 @@ edb.padstacks.place([35e-3, -5e-3], "MyVia")
 edb.padstacks.place([45e-3, -5e-3], "MyVia")
 
 
-#######################################
-# Geometry Plot
-# ~~~~~~~~~~~~~
-#
+# ## Generate geometry plot
+
 edb.nets.plot(None, color_by_net=True)
 
-#######################################
-# Stackup Plot
-# ~~~~~~~~~~~~
-#
+# ## Generate stackup plot
+
 edb.stackup.plot(plot_definitions="MyVia")
 
-
-####################
-# Save and close EDB
-# ~~~~~~~~~~~~~~~~~~
+# ## Save and close EDB
 # Save and close EDB.
 
 if edb:
     edb.save_edb()
     edb.close_edb()
 print("EDB saved correctly to {}. You can import in AEDT.".format(aedb_path))
-end = time.time() - start
-print(end)
+
+# ### Clean up temporary directory
+#
+# The following command removes the project and the temporary directory.
+# If you'd like to save this project, save it to a folder of your choice
+# prior to running the following cell.
+
+temp_dir.cleanup()
