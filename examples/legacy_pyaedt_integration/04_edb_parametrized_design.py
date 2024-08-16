bfd27fc09c457cc12ed2d30359e44839efb84e9e
ring630 <@gmail.com>
2024-08-16 15:38:57 +0200

    FIX: create differential pair

--- examples/legacy_pyaedt_integration/04_edb_parametrized_design.py
+++ examples/legacy_pyaedt_integration/04_edb_parametrized_design.py
@@ -34,7 +34,7 @@ temp_dir = tempfile.TemporaryDirectory(suffix=".ansys")
 aedb_path = os.path.join(temp_dir.name, "pcb.aedb")
 
 # Select EDB version (change it manually if needed, e.g. "2024.1")
-edb_version = "2024.2"
+edb_version = "2024.1"
 print(f"EDB version: {edb_version}")
 
 edb = pyedb.Edb(edbpath=aedb_path, edbversion=edb_version)
@@ -288,7 +288,7 @@ edb.close_edb()
 
 h3d = pyaedt.Hfss3dLayout(
     projectname=aedb_path,
-    specified_version="2024.2",
+    specified_version="2024.1",
     non_graphical=non_graphical,
     new_desktop_session=True,
 )
