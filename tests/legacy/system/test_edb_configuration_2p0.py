a117df473953c8aa2f48b1a45d509ee81139a499
ring630 <@gmail.com>
2024-10-03 12:13:10 +0200

    fix

--- tests/legacy/system/test_edb_configuration_2p0.py
+++ tests/legacy/system/test_edb_configuration_2p0.py
@@ -827,14 +827,6 @@ class TestClass:
         }
         edbapp = edb_examples.get_si_verse()
         assert edbapp.configuration.load(data, apply_file=True)
-        """data_from_db = edbapp.configuration.get_data_from_db(setups=True)
-        for setup in data["setups"]:
-            target = [i for i in data_from_db["setups"] if i["name"] == setup["name"]][0]
-            for p, value in setup.items():
-                if p == "freq_sweep":
-                    pass  # EDB API bug. Cannot retrieve frequency sweep from edb.
-                else:
-                    assert value == target[p]"""
         edbapp.close()
 
     def test_15b_sources_net_net(self, edb_examples):
