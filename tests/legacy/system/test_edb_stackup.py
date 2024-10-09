654040583d750dbc94af37809549a6043aecdfbe
Hui Zhou <ring630@gmail.com>
2024-10-09 10:08:56 +0200

    FEAT: edb cfg roughness (#844)
    * feat: edb cfg roughness
    
    * MISC: Auto fixes from pre-commit.com hooks
    
    For more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: ring630 <@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/legacy/system/test_edb_stackup.py
+++ tests/legacy/system/test_edb_stackup.py
@@ -1117,3 +1117,24 @@ class TestClass:
         base_layer = edbapp.stackup.layers["1_Top"]
         l_id = edbapp.stackup.layers_by_id.index([base_layer.id, base_layer.name])
         assert edbapp.stackup.layers_by_id[l_id - 1][1] == "add_layer_above"
+
+    def test_20_layer_properties(self, edb_examples):
+        edbapp = edb_examples.get_si_verse()
+        data = {
+            "name": "1_Top",
+            "type": "signal",
+            "material": "copper",
+            "fill_material": "Solder Resist",
+            "thickness": "0.03mm",
+            "color": [255, 0, 0],
+            "roughness": {
+                "top": {"model": "huray", "nodule_radius": "0.1um", "surface_ratio": "1"},
+                "bottom": {"model": "groisse", "roughness": "2um"},
+                "side": {"model": "huray", "nodule_radius": "0.5um", "surface_ratio": "2.9"},
+                "enabled": True,
+            },
+        }
+        edbapp.stackup.layers["1_Top"].properties = data
+        layer_data = edbapp.stackup.layers["1_Top"].properties
+        assert layer_data == data
+        edbapp.close()
