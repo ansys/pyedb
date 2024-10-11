ac0e8da3f5be0dd0ff54581f2a480aa396e874ce
Julien Dufrenne <julien.dufrenne@ansys.com>
2024-10-11 09:55:07 +0200

    #850 : Add custom title argument to plot function

--- src/pyedb/dotnet/edb_core/nets.py
+++ src/pyedb/dotnet/edb_core/nets.py
@@ -713,6 +713,7 @@ class EdbNets(object):
         plot_components_on_top=False,
         plot_components_on_bottom=False,
         show=True,
+        title=None,
     ):
         """Plot a Net to Matplotlib 2D Chart.
 
@@ -762,6 +763,10 @@ class EdbNets(object):
             fig_size_x = size
             fig_size_y = board_size_y * fig_size_x / board_size_x
             size = (fig_size_x, fig_size_y)
+            
+        plot_title=title
+        if plot_title is None:
+            plot_title=self._pedb.active_cell.GetName()
 
         return plot_matplotlib(
             plot_data=object_lists,
@@ -769,7 +774,7 @@ class EdbNets(object):
             show_legend=show_legend,
             xlabel="X (m)",
             ylabel="Y (m)",
-            title=self._pedb.active_cell.GetName(),
+            title=plot_title,
             save_plot=save_plot,
             axis_equal=True,
             show=show,
