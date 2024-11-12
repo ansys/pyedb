5f1ce9c8ccbf3142609a0ca586a3fa7b76ce4272
svandenb-dev <simon.vandenbrouck@ansys.com>
2024-11-12 15:03:44 +0100

    test #ipc

--- tests/grpc/system/test_edb_ipc.py
+++ tests/grpc/system/test_edb_ipc.py
@@ -28,44 +28,34 @@ import os
 import pytest
 
 from pyedb.dotnet.edb import Edb
-from tests.conftest import desktop_version, local_path
-from tests.legacy.system.conftest import test_subfolder
+from tests.conftest import desktop_version
 
 pytestmark = [pytest.mark.system, pytest.mark.legacy]
 
 
 class TestClass:
     @pytest.fixture(autouse=True)
-    def init(self, legacy_edb_app, local_scratch, target_path, target_path2, target_path4):
-        self.edbapp = legacy_edb_app
+    def init(self, local_scratch, target_path, target_path2, target_path4):
         self.local_scratch = local_scratch
         self.target_path = target_path
         self.target_path2 = target_path2
         self.target_path4 = target_path4
 
-    def test_export_to_ipc2581_0(self):
+    def test_export_to_ipc2581_0(self, edb_examples):
         """Export of a loaded aedb file to an XML IPC2581 file"""
-        source_path = os.path.join(local_path, "example_models", test_subfolder, "ANSYS-HSD_V1_cut.aedb")
-        target_path = os.path.join(self.local_scratch.path, "ANSYS-HSD_V1_ipc.aedb")
-        self.local_scratch.copyfolder(source_path, target_path)
-        edbapp = Edb(target_path, edbversion=desktop_version)
-        xml_file = os.path.join(self.local_scratch.path, "test.xml")
+        edbapp = edb_examples.get_si_verse()
+        xml_file = os.path.join(edbapp.directory, "test.xml")
         edbapp.export_to_ipc2581(xml_file)
         assert os.path.exists(xml_file)
-
-        # Export should be made with units set to default -millimeter-.
         edbapp.export_to_ipc2581(xml_file, "mm")
         assert os.path.exists(xml_file)
         edbapp.close()
 
     @pytest.mark.xfail(reason="This test is expected to crash (sometimes) at `ipc_edb.close()`")
-    def test_export_to_ipc2581_1(self):
+    def test_export_to_ipc2581_1(self, edb_examples):
         """Export of a loaded aedb file to an XML IPC2581 file"""
-        source_path = os.path.join(local_path, "example_models", test_subfolder, "ANSYS-HSD_V1.aedb")
-        target_path = os.path.join(self.local_scratch.path, "test_ipc", "ANSYS-HSD_V1_boundaries.aedb")
-        self.local_scratch.copyfolder(source_path, target_path)
-        edbapp = Edb(target_path, edbversion=desktop_version)
-        xml_file = os.path.join(target_path, "test.xml")
+        edbapp = edb_examples.get_si_verse()
+        xml_file = os.path.join(edbapp.directory, "test.xml")
         edbapp.export_to_ipc2581(xml_file)
         edbapp.close()
         assert os.path.isfile(xml_file)
