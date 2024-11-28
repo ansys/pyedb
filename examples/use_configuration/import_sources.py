fef6470ef0b923831f21e2c5873baf58148a8962
ring630 <@gmail.com>
2024-11-28 12:10:49 +0100

    add icepak design

--- examples/use_configuration/import_sources.py
+++ examples/use_configuration/import_sources.py
@@ -55,6 +55,7 @@ voltage_source = {
     "name": "V_SOURCE_5V",
     "reference_designator": "U4",
     "type": "voltage",
+    "magnitude": 1,
     "positive_terminal": {"net": "5V"},
     "negative_terminal": {"net": "GND"},
 }
@@ -65,6 +66,7 @@ current_source_1 = {
     "name": "I_CURRENT_1A",
     "reference_designator": "J5",
     "type": "current",
+    "magnitude": 10,
     "positive_terminal": {"pin": "15"},
     "negative_terminal": {"pin": "14"},
 }
--- examples/use_configuration/pcb_dc_ir.py
+++ examples/use_configuration/pcb_dc_ir.py
@@ -9,11 +9,11 @@ import json
 import os
 import tempfile
 
-from IPython.display import display
-from ansys.aedt.core import Hfss3dLayout
+from ansys.aedt.core import Hfss3dLayout, Icepak
 from ansys.aedt.core.downloads import download_file
 
 from pyedb import Edb
+
 
 AEDT_VERSION = "2024.2"
 NG_MODE = False
@@ -34,6 +34,36 @@ edbapp = Edb(file_edb, edbversion=AEDT_VERSION)
 cfg = dict()
 cfg["sources"] = []
 
+# ## Update stackup
+
+cfg["stackup"] = {
+    "layers": [
+        {"name": "Top", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.035mm"},
+        {"name": "DE1", "type": "dielectric", "material": "FR4_epoxy", "fill_material": "", "thickness": "0.1mm"},
+        {"name": "Inner1", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.017mm"},
+        {"name": "DE2", "type": "dielectric", "material": "FR4_epoxy", "fill_material": "", "thickness": "0.088mm"},
+        {"name": "Inner2", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.017mm"},
+        {"name": "DE3", "type": "dielectric", "material": "FR4_epoxy", "fill_material": "", "thickness": "0.1mm"},
+        {"name": "Inner3", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.017mm"},
+        {"name": "FR4_epoxy-1mm", "type": "dielectric", "material": "FR4_epoxy", "fill_material": "",
+         "thickness": "1mm"},
+        {"name": "Inner4", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.017mm"},
+        {"name": "DE5", "type": "dielectric", "material": "FR4_epoxy", "fill_material": "", "thickness": "0.1mm"},
+        {"name": "Inner5", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.017mm"},
+        {"name": "DE6", "type": "dielectric", "material": "FR4_epoxy", "fill_material": "", "thickness": "0.088mm"},
+        {"name": "Inner6", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.017mm"},
+        {"name": "DE7", "type": "dielectric", "material": "FR4_epoxy", "fill_material": "", "thickness": "0.1mm"},
+        {"name": "Bottom", "type": "signal", "material": "copper", "fill_material": "FR4_epoxy",
+         "thickness": "0.035mm"}
+    ]}
+
 # ## Define voltage source
 
 cfg["sources"].append(
@@ -41,6 +71,7 @@ cfg["sources"].append(
         "name": "vrm",
         "reference_designator": "U2",
         "type": "voltage",
+        "magnitude": 1,
         "positive_terminal": {"net": "1V0"},
         "negative_terminal": {"net": "GND"},
     }
@@ -53,6 +84,7 @@ cfg["sources"].append(
         "name": "U1_1V0",
         "reference_designator": "U1",
         "type": "current",
+        "magnitude": 10,
         "positive_terminal": {"net": "1V0"},
         "negative_terminal": {"net": "GND"},
     }
@@ -76,8 +108,25 @@ cfg["operations"] = {
         "signal_list": ["1V0"],
         "reference_list": ["GND"],
         "extent_type": "ConvexHull",
+        "expansion_size": "20mm"
     }
 }
+
+# ## Define package for thermal analysis (optional)
+
+cfg["package_definitions"] = [
+    {
+        "name": "package_1",
+        "component_definition": "ALTR-FBGA1517-Ansys",
+        "maximum_power": 0.5,
+        "therm_cond": 2,
+        "theta_jb": 3,
+        "theta_jc": 4,
+        "height": "1mm",
+        "apply_to_all": False,
+        "components": ["U1"],
+    },
+]
 
 # ## Write configuration into as json file
 
@@ -106,6 +155,14 @@ print(temp_folder.name)
 
 h3d = Hfss3dLayout(edbapp.edbpath, version=AEDT_VERSION, non_graphical=NG_MODE, new_desktop=True)
 
+# ## Prepare for electro-thermal analysis in Icepak (Optional)
+
+h3d.modeler.set_temperature_dependence(
+    include_temperature_dependence=True,
+    enable_feedback=True,
+    ambient_temp=22
+)
+
 # ## Analyze
 
 h3d.analyze()
@@ -114,16 +171,17 @@ h3d.analyze()
 
 voltage = h3d.post.create_fieldplot_layers_nets(
     layers_nets=[
-        ["1_Top", "1V0"],
+        ["Inner2", "1V0"],
     ],
     quantity="Voltage",
     setup="siwave_1",
 )
+
 file_path_image = os.path.join(temp_folder.name, "voltage.jpg")
 voltage.export_image(
     full_path=file_path_image,
-    width=1920,
-    height=1080,
+    width=640,
+    height=480,
     orientation="isometric",
     display_wireframe=True,
     selections=None,
@@ -137,7 +195,7 @@ voltage.export_image(
 
 power_density = h3d.post.create_fieldplot_layers_nets(
     layers_nets=[
-        ["1_Top", "1V0"],
+        ["Inner2", "no-net"],
     ],
     quantity="Power Density",
     setup="siwave_1",
@@ -146,8 +204,8 @@ power_density = h3d.post.create_fieldplot_layers_nets(
 file_path_image = os.path.join(temp_folder.name, "power_density.jpg")
 power_density.export_image(
     full_path=file_path_image,
-    width=1920,
-    height=1080,
+    width=640,
+    height=480,
     orientation="isometric",
     display_wireframe=True,
     selections=None,
@@ -159,15 +217,94 @@ power_density.export_image(
 
 # ## Compute power loss
 
-p_layers = h3d.post.compute_power_by_layer(layers=["1_Top"])
-display(p_layers)
+p_layers = h3d.post.compute_power_by_layer(layers=["Top"])
+print(p_layers)
 
 p_nets = h3d.post.compute_power_by_nets(nets=["1V0"])
-display(p_nets)
+print(p_nets)
+
+# ## Save HFSS 3D Layout project
+
+h3d.save_project()
+
+# ## Create an Icepak design
+
+ipk = Icepak(
+    version=AEDT_VERSION,
+    non_graphical=NG_MODE,
+    new_desktop=False
+)
+
+# ## Create PCB
+
+pcb = ipk.create_ipk_3dcomponent_pcb(
+    compName="PCB_pyAEDT",
+    setupLinkInfo=[h3d.project_file,
+                   h3d.design_name,
+                   "siwave_1",
+                   True,
+                   True],
+
+    solutionFreq=None,
+    resolution=0,
+    extent_type="Bounding Box",
+    powerin="0"
+)
+
+# ## Include pckage definition from Edb
+
+pcb.included_parts = "Device"
+
+# ## Adjust air region
+
+region = ipk.modeler["Region"]
+faces = [f.id for f in region.faces]
+ipk.assign_pressure_free_opening(assignment=faces, boundary_name="Outlet")
+
+# ## Setup mesh
+
+glob_msh = ipk.mesh.global_mesh_region
+glob_msh.global_region.positive_z_padding_type = "Absolute Offset"
+glob_msh.global_region.positive_z_padding = "50 mm"
+glob_msh.global_region.negative_z_padding_type = "Absolute Offset"
+glob_msh.global_region.negative_z_padding = "80 mm"
+
+glob_msh = ipk.mesh.global_mesh_region
+glob_msh.manual_settings = True
+glob_msh.settings["EnableMLM"] = True
+glob_msh.settings["EnforeMLMType"] = '2D'
+glob_msh.settings["2DMLMType"] = 'Auto'
+glob_msh.settings["MaxElementSizeY"] = '2mm'
+glob_msh.settings["MaxElementSizeX"] = '2mm'
+glob_msh.settings["MaxElementSizeZ"] = '3mm'
+glob_msh.settings["MaxLevels"] = "2"
+
+glob_msh.update()
+
+# ## Place monitor
+
+cpu = ipk.modeler["PCB_pyAEDT_U1_device"]
+m1 = ipk.monitor.assign_face_monitor(
+    face_id=cpu.top_face_z.id,
+    monitor_quantity="Temperature",
+    monitor_name="TemperatureMonitor1",
+)
+
+# ## Create Icepak setup
+
+setup1 = ipk.create_setup(MaxIterations=10)
+
+# Add 2-way coupling to the setup
+
+ipk.assign_2way_coupling(number_of_iterations=1)
+
+# ## Save
+
+ipk.save_project()
 
 # ## Shut Down Electronics Desktop
 
-h3d.close_desktop()
+ipk.release_desktop()
 
 # All project files are saved in the folder ``temp_file.dir``. If you've run this example as a Jupyter notebook you
 # can retrieve those project files.
