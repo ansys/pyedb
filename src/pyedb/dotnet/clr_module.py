ef19d7ed8851795f5dff3b20d948df21012cd3f7
Sebastien Morais <sebastien.morais@ansys.com>
2024-12-02 15:16:46 +0100

    WIP

--- src/pyedb/dotnet/clr_module.py
+++ src/pyedb/dotnet/clr_module.py
@@ -47,6 +47,7 @@ if is_linux:  # pragma: no cover
 
             runtime = get_coreclr()
             load(runtime)
+            os.environ["DOTNET_ROOT"] = runtime.dotnet_root
             is_clr = True
         # TODO: Fall backing to dotnetcore2 should be removed in a near future.
         except Exception:
