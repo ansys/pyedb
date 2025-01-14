e804b3ac54406a6a719d4ebf91e9c9dfb69c80c4
Hui Zhou <ring630@gmail.com>
2025-01-14 19:47:45 +0100

    REFACTOR: layer property (#980)
    * REFACCTOR: layer property
    
    * MISC: Auto fixes from pre-commit.com hooks
    
    For more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: ring630 <@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/pyedb/dotnet/edb_core/cell/terminal/padstack_instance_terminal.py
+++ src/pyedb/dotnet/edb_core/cell/terminal/padstack_instance_terminal.py
@@ -101,3 +101,15 @@ class PadstackInstanceTerminal(Terminal):
     def padstack_instance(self):
         p_inst, _ = self._get_parameters()
         return self._pedb.layout.find_object_by_id(p_inst.GetId())
+
+    @property
+    def layer(self):
+        """Get layer of the terminal."""
+        _, _, layer = self._edb_object.GetParameters()
+        return self._pedb.stackup.all_layers[layer.GetName()]
+
+    @layer.setter
+    def layer(self, value):
+        layer = self._pedb.stackup.layers[value]._edb_layer
+        point_data = self._pedb.point_data(*self.location)
+        self._edb_object.SetParameters(point_data, layer)
