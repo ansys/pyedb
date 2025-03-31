e5c90e42bb1e1a64a93cd5b595e3d375f1df43a2
svandenb-dev <simon.vandenbrouck@ansys.com>
2025-03-31 14:19:25 +0200

    grpc transition PR#946 tracker

--- tests/grpc/system/test_edb_padstacks.py
+++ tests/grpc/system/test_edb_padstacks.py
@@ -512,3 +512,15 @@ class TestClass:
         # assert len(result) == 1
         # edbapp.close()
         pass
+
+    def test_reduce_via_in_bounding_box(self):
+        source_path = os.path.join(local_path, "example_models", test_subfolder, "vias_300.aedb")
+        edbapp = Edb(edbpath=source_path, edbversion=desktop_version)
+        assert len(edbapp.padstacks.instances) == 301
+        # empty bounding box
+        assert edbapp.padstacks.reduce_via_in_bounding_box([-16e-3, -7e-3, -13e-3, -6e-3], 10, 10) is False
+        # over sampling
+        assert edbapp.padstacks.reduce_via_in_bounding_box([-20e-3, -10e-3, 20e-3, 10e-3], 20, 20) is False
+        assert edbapp.padstacks.reduce_via_in_bounding_box([-20e-3, -10e-3, 20e-3, 10e-3], 10, 10) is True
+        assert len(edbapp.padstacks.instances) == 96
+        edbapp.close_edb()
