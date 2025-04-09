e16e8c741643b95171a8e8cb10dd4f752e1dce9c
svandenb-dev <simon.vandenbrouck@ansys.com>
2025-04-09 14:49:16 +0200

    config file spice model

--- src/pyedb/grpc/database/hierarchy/s_parameter_model.py
+++ src/pyedb/grpc/database/hierarchy/s_parameter_model.py
@@ -28,6 +28,6 @@ from ansys.edb.core.hierarchy.sparameter_model import (
 class SparamModel(GrpcSParameterModel):  # pragma: no cover
     """Manage :class:`SParameterModel <ansys.edb.core.hierarchy.sparameter_model.SParameterModel>`"""
 
-    def __init__(self, edb_model):
+    def __init__(self, edb_object):
         super().__init__(self.msg)
-        self._edb_model = edb_model
+        self._edb_model = edb_object
