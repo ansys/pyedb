877178327d983b5453580ee8b03f7885cfa6b1d5
svandenb-dev <simon.vandenbrouck@ansys.com>
2025-05-21 15:41:49 +0200

    test grpc compatibility edb diff pairs

--- tests/legacy/system/test_edb_differential_pairs.py
+++ tests/legacy/system/test_edb_differential_pairs.py
@@ -30,17 +30,19 @@ pytestmark = [pytest.mark.system, pytest.mark.legacy]
 
 class TestClass:
     @pytest.fixture(autouse=True)
-    def init(self, legacy_edb_app, local_scratch, target_path, target_path2, target_path4):
-        self.edbapp = legacy_edb_app
+    def init(self, local_scratch, target_path, target_path2, target_path4):
         self.local_scratch = local_scratch
         self.target_path = target_path
         self.target_path2 = target_path2
         self.target_path4 = target_path4
 
-    def test_differential_pairs_queries(self):
+    def test_differential_pairs_queries(self, edb_examples):
         """Evaluate differential pairs queries"""
-        self.edbapp.differential_pairs.auto_identify()
-        diff_pair = self.edbapp.differential_pairs.create("new_pair1", "PCIe_Gen4_RX1_P", "PCIe_Gen4_RX1_N")
+        # Done
+        edbapp = edb_examples.get_si_verse()
+        edbapp.differential_pairs.auto_identify()
+        diff_pair = edbapp.differential_pairs.create("new_pair1", "PCIe_Gen4_RX1_P", "PCIe_Gen4_RX1_N")
         assert diff_pair.positive_net.name == "PCIe_Gen4_RX1_P"
         assert diff_pair.negative_net.name == "PCIe_Gen4_RX1_N"
-        assert self.edbapp.differential_pairs["new_pair1"]
+        assert edbapp.differential_pairs.items["new_pair1"]
+        edbapp.close()
