528813a86865aea0aecb8639a19e070e0117dfea
svandenb-dev <simon.vandenbrouck@ansys.com>
2025-07-01 14:47:45 +0200

    cpa data class

--- src/pyedb/siwave_core/cpa/simulation_setup.py
+++ src/pyedb/siwave_core/cpa/simulation_setup.py
@@ -5,8 +5,35 @@ from dataclasses_json import dataclass_json
 
 @dataclass_json
 @dataclass
+class DcParameter:
+    rl: bool = True
+    cg: bool = True
+
+
+@dataclass_json
+@dataclass
+class AdaptiveSettings:
+    cg_max_passes: int = 10
+    cg_percent_error: float = 0.02
+    cg_percent_refinement_per_pass: float = 0.33
+    rl_max_passes: int = 10
+    rl_percent_error: float = 0.02
+    rl_percent_refinement_per_pass: float = 0.33
+
+
+@dataclass_json
+@dataclass
 class SolverOptions:
     extraction_mode: str = "si"
+    custom_refinement: bool = False
+    extraction_frequency: str = "10Ghz"
+    compute_capacitance: bool = True
+    compute_dc_parameters: bool = True
+    dc_parameters: DcParameter = None
+    compute_ac_rl: bool = True
+    ground_power_ground_nets_for_si: bool = False
+    small_hole_diameter: str = "auto"
+    adaptive_settings: AdaptiveSettings = None
 
 
 @dataclass_json
@@ -19,19 +46,22 @@ class Vrm:
 
 @dataclass_json
 @dataclass
-class ChannelConfiguration:
+class ChannelSetup:
     die_name: str = ""
     pin_grouping_mode: str = "perpin"  # usediepingroups and ploc are supported
-    channel_comp_exposure: list[str] = field(default_factory=list)
+    channel_component_exposure: list[str] = field(default_factory=list)
     expose: bool = True
+    vrm_setup: Vrm = None
 
 
 @dataclass_json
 @dataclass
-class SIwaveCPAProperties:
+class SIwaveCpaSetup:
     name: str = ""
     mode: str = "channel"
     model_type: str = "rlcg"
     use_q3d_solcer: bool = True
     net_processing_mode: str = "userspecified"
-    return_path_net_for_loop_params: str = ""
+    return_path_net_for_loop_parameters: str = ""
+    channel_setup: ChannelSetup = None
+    solver_options: SolverOptions = None
