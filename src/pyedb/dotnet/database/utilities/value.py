2b53ec560d225246e491a94ff28dabcd6c2f9dcf
ring630 <@gmail.com>
2025-07-14 17:32:01 +0200

    fix

--- src/pyedb/dotnet/database/utilities/value.py
+++ src/pyedb/dotnet/database/utilities/value.py
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import numpy as np
-
 
 class Value(float):
     """Class defining Edb Value properties."""
