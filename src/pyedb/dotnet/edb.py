e4542cf7fca2ff9029c1c8a6f7f97de28438cf49
gkorompi <george.korompilis@ansys.com>
2025-07-25 15:27:19 +0300

    FEAT: vlctech

--- src/pyedb/dotnet/edb.py
+++ src/pyedb/dotnet/edb.py
@@ -860,9 +860,7 @@ class Edb:
         """
         if not working_dir:
             working_dir = os.path.dirname(vlctech_file)
-        command = os.path.join(self.base_path, "helic", "tools", "raptorh", "bin", "make-edb")
-        if is_windows:
-            command += ".exe"
+        command = os.path.join(self.base_path, "helic", "tools", "raptorh", "bin", "make-edb.exe")
         cmd_make_edb = [
             command,
             "-t",
@@ -870,6 +868,9 @@ class Edb:
             "-o",
             "{}".format(os.path.join(working_dir, "vlctech")),
         ]
+        if is_linux:
+            mono_path = os.path.join(self.base_path, "common/mono/Linux64/bin/mono")
+            cmd_make_edb.insert(0, mono_path)
         if export_xml:
             cmd_make_edb.extend(["-x", "{}".format(export_xml)])
         subprocess.run(cmd_make_edb)
