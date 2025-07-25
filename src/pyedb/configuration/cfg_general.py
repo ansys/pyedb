af791a451a5836c7267187d3003df29cc56ebce3
hui-zhou-a <huizhou@synopsys.com>
2025-07-25 17:15:45 +0200

    REFACTOR: revert general

--- src/pyedb/configuration/cfg_general.py
+++ src/pyedb/configuration/cfg_general.py
@@ -24,50 +24,27 @@
 class CfgGeneral:
     """Manage configuration general settings."""
 
-    class Grpc:
-        def __init__(self, parent):
-            self.parent = parent
-            self.pedb = parent.pedb
+    def set_parameters_to_edb(self):
+        if self.anti_pads_always_on is not None:
+            self.pedb.design_options.anti_pads_always_on = self.anti_pads_always_on
+        if self.suppress_pads is not None:
+            self.pedb.design_options.suppress_pads = self.suppress_pads
 
-        def set_parameters_to_edb(self):
-            self.pedb.active_cell.anti_pads_always_on = self.parent.anti_pads_always_on
-            self.pedb.active_cell.suppress_pads = self.parent.suppress_pads
-
-        def get_parameters_from_edb(self):
-            anti_pads_always_on = self.pedb.active_cell.anti_pads_always_on
-            suppress_pads = self.pedb.active_cell.suppress_pads
-            data = {"anti_pads_always_on": anti_pads_always_on, "suppress_pads": suppress_pads}
-            return data
-
-    class DotNet(Grpc):
-        def __init__(self, parent):
-            super().__init__(parent)
-
-        def set_parameters_to_edb(self):
-            if self.parent.anti_pads_always_on is not None:
-                self.pedb.design_options.anti_pads_always_on = self.parent.anti_pads_always_on
-            if self.parent.suppress_pads is not None:
-                self.pedb.design_options.suppress_pads = self.parent.suppress_pads
-
-        def get_parameters_from_edb(self):
-            anti_pads_always_on = self.pedb.design_options.antipads_always_on
-            suppress_pads = self.pedb.design_options.suppress_pads
-            data = {"anti_pads_always_on": anti_pads_always_on, "suppress_pads": suppress_pads}
-            return data
+    def get_parameters_from_edb(self):
+        anti_pads_always_on = self.pedb.design_options.antipads_always_on
+        suppress_pads = self.pedb.design_options.suppress_pads
+        data = {"anti_pads_always_on": anti_pads_always_on, "suppress_pads": suppress_pads}
+        return data
 
     def __init__(self, pedb, data):
         self.pedb = pedb
-        if self.pedb.grpc:
-            self.api = self.Grpc(self)
-        else:
-            self.api = self.DotNet(self)
         self.spice_model_library = data.get("spice_model_library", "")
         self.s_parameter_library = data.get("s_parameter_library", "")
         self.anti_pads_always_on = data.get("anti_pads_always_on", None)
         self.suppress_pads = data.get("suppress_pads", None)
 
     def apply(self):
-        self.api.set_parameters_to_edb()
+        self.set_parameters_to_edb()
 
     def get_data_from_db(self):
-        return self.api.get_parameters_from_edb()
+        return self.get_parameters_from_edb()
