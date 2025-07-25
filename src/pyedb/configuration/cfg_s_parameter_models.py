f7ad3f6ce458b103da35c4a447a066a5983e3eac
hui-zhou-a <huizhou@synopsys.com>
2025-07-25 18:03:30 +0200

    revert sparemeter

--- src/pyedb/configuration/cfg_s_parameter_models.py
+++ src/pyedb/configuration/cfg_s_parameter_models.py
@@ -36,186 +36,88 @@ class CfgSParameterModel:
 
 
 class CfgSParameters:
-    class Grpc:
-        def __init__(self, parent):
-            self.parent = parent
-            self._pedb = parent._pedb
 
-        def apply(self):
-            for s_param in self.parent.s_parameters_models:
-                fpath = s_param.file_path
-                if not Path(fpath).anchor:
-                    fpath = str(Path(self.parent.path_libraries) / fpath)
-                comp_def = self._pedb.definitions.component[s_param.component_definition]
-                if s_param.pin_order:
-                    comp_def.set_properties(pin_order=s_param.pin_order)
-                comp_def.add_n_port_model(fpath, s_param.name)
-                comp_list = dict()
-                if s_param.apply_to_all:
-                    comp_list.update(
-                        {
-                            refdes: comp
-                            for refdes, comp in comp_def.components.items()
-                            if refdes not in s_param.components
-                        }
-                    )
-                else:
-                    comp_list.update(
-                        {refdes: comp for refdes, comp in comp_def.components.items() if refdes in s_param.components}
-                    )
-
-                for refdes, comp in comp_list.items():
-                    if refdes in s_param.reference_net_per_component:
-                        ref_net = s_param.reference_net_per_component[refdes]
-                    else:
-                        ref_net = s_param.reference_net
-                    comp.use_s_parameter_model(s_param.name, reference_net=ref_net)
-
-        def get_data_from_db(self, cfg_components):
-            db_comp_def = self._pedb.definitions.component
-            for name, compdef_obj in db_comp_def.items():
-                nport_models = compdef_obj.component_models
-                if not nport_models:
-                    continue
-                else:
-                    pin_order = compdef_obj.get_properties()["pin_order"]
-                    temp_comps = [i for i in cfg_components if i["definition"] == name]
-                    for model_obj in nport_models:
-                        temp_comp_list = []
-                        reference_net_per_component = {}
-                        for i in temp_comps:
-                            s_param_model = i.get("s_parameter_model")
-                            if s_param_model:
-                                if s_param_model["model_name"] == model_name:
-                                    temp_comp_list.append(i["reference_designator"])
-                                    reference_net_per_component[i["reference_designator"]] = s_param_model[
-                                        "reference_net"
-                                    ]
-                            else:
-                                continue
-
-                        self.parent.s_parameters_models.append(
-                            CfgSParameterModel(
-                                name=model_name,
-                                component_definition=name,
-                                file_path=model_obj.reference_file,
-                                apply_to_all=False,
-                                components=temp_comp_list,
-                                reference_net_per_component=reference_net_per_component,
-                                pin_order=pin_order,
-                            )
-                        )
-
-            data = []
-            for i in self.parent.s_parameters_models:
-                data.append(
+    def apply(self):
+        for s_param in self.s_parameters_models:
+            fpath = s_param.file_path
+            if not Path(fpath).anchor:
+                fpath = str(Path(self.path_libraries) / fpath)
+            comp_def = self._pedb.definitions.component[s_param.component_definition]
+            if s_param.pin_order:
+                comp_def.set_properties(pin_order=s_param.pin_order)
+            comp_def.add_n_port_model(fpath, s_param.name)
+            comp_list = dict()
+            if s_param.apply_to_all:
+                comp_list.update(
                     {
-                        "name": i.name,
-                        "component_definition": i.component_definition,
-                        "file_path": i.file_path,
-                        "apply_to_all": i.apply_to_all,
-                        "components": i.components,
-                        "reference_net_per_component": i.reference_net_per_component,
-                        "pin_order": i.pin_order,
+                        refdes: comp
+                        for refdes, comp in comp_def.components.items()
+                        if refdes not in s_param.components
                     }
                 )
-            return data
-
-    class DotNet(Grpc):
-        def __init__(self, parent):
-            super().__init__(parent)
-
-        def apply(self):
-            for s_param in self.parent.s_parameters_models:
-                fpath = s_param.file_path
-                if not Path(fpath).anchor:
-                    fpath = str(Path(self.parent.path_libraries) / fpath)
-                comp_def = self._pedb.definitions.component[s_param.component_definition]
-                if s_param.pin_order:
-                    comp_def.set_properties(pin_order=s_param.pin_order)
-                comp_def.add_n_port_model(fpath, s_param.name)
-                comp_list = dict()
-                if s_param.apply_to_all:
-                    comp_list.update(
-                        {
-                            refdes: comp
-                            for refdes, comp in comp_def.components.items()
-                            if refdes not in s_param.components
-                        }
-                    )
-                else:
-                    comp_list.update(
-                        {refdes: comp for refdes, comp in comp_def.components.items() if refdes in s_param.components}
-                    )
-
-                for refdes, comp in comp_list.items():
-                    if refdes in s_param.reference_net_per_component:
-                        ref_net = s_param.reference_net_per_component[refdes]
-                    else:
-                        ref_net = s_param.reference_net
-                    comp.use_s_parameter_model(s_param.name, reference_net=ref_net)
-
-        def get_data_from_db(self, cfg_components):
-            db_comp_def = self._pedb.definitions.component
-            for name, compdef_obj in db_comp_def.items():
-                nport_models = compdef_obj.component_models
-                if not nport_models:
-                    continue
-                else:
-                    pin_order = compdef_obj.get_properties()["pin_order"]
-                    temp_comps = [i for i in cfg_components if i["definition"] == name]
-                    for model_name, model_obj in nport_models.items():
-                        temp_comp_list = []
-                        reference_net_per_component = {}
-                        for i in temp_comps:
-                            s_param_model = i.get("s_parameter_model")
-                            if s_param_model:
-                                if s_param_model["model_name"] == model_name:
-                                    temp_comp_list.append(i["reference_designator"])
-                                    reference_net_per_component[i["reference_designator"]] = s_param_model[
-                                        "reference_net"
-                                    ]
-                            else:
-                                continue
-
-                        self.parent.s_parameters_models.append(
-                            CfgSParameterModel(
-                                name=model_name,
-                                component_definition=name,
-                                file_path=model_obj.reference_file,
-                                apply_to_all=False,
-                                components=temp_comp_list,
-                                reference_net_per_component=reference_net_per_component,
-                                pin_order=pin_order,
-                            )
-                        )
-
-            data = []
-            for i in self.parent.s_parameters_models:
-                data.append(
-                    {
-                        "name": i.name,
-                        "component_definition": i.component_definition,
-                        "file_path": i.file_path,
-                        "apply_to_all": i.apply_to_all,
-                        "components": i.components,
-                        "reference_net_per_component": i.reference_net_per_component,
-                        "pin_order": i.pin_order,
-                    }
+            else:
+                comp_list.update(
+                    {refdes: comp for refdes, comp in comp_def.components.items() if refdes in s_param.components}
                 )
-            return data
+
+            for refdes, comp in comp_list.items():
+                if refdes in s_param.reference_net_per_component:
+                    ref_net = s_param.reference_net_per_component[refdes]
+                else:
+                    ref_net = s_param.reference_net
+                comp.use_s_parameter_model(s_param.name, reference_net=ref_net)
+
+    def get_data_from_db(self, cfg_components):
+        db_comp_def = self._pedb.definitions.component
+        for name, compdef_obj in db_comp_def.items():
+            nport_models = compdef_obj.component_models
+            if not nport_models:
+                continue
+            else:
+                pin_order = compdef_obj.get_properties()["pin_order"]
+                temp_comps = [i for i in cfg_components if i["definition"] == name]
+                for model_name, model_obj in nport_models.items():
+                    temp_comp_list = []
+                    reference_net_per_component = {}
+                    for i in temp_comps:
+                        s_param_model = i.get("s_parameter_model")
+                        if s_param_model:
+                            if s_param_model["model_name"] == model_name:
+                                temp_comp_list.append(i["reference_designator"])
+                                reference_net_per_component[i["reference_designator"]] = s_param_model[
+                                    "reference_net"
+                                ]
+                        else:
+                            continue
+
+                    self.s_parameters_models.append(
+                        CfgSParameterModel(
+                            name=model_name,
+                            component_definition=name,
+                            file_path=model_obj.reference_file,
+                            apply_to_all=False,
+                            components=temp_comp_list,
+                            reference_net_per_component=reference_net_per_component,
+                            pin_order=pin_order,
+                        )
+                    )
+
+        data = []
+        for i in self.s_parameters_models:
+            data.append(
+                {
+                    "name": i.name,
+                    "component_definition": i.component_definition,
+                    "file_path": i.file_path,
+                    "apply_to_all": i.apply_to_all,
+                    "components": i.components,
+                    "reference_net_per_component": i.reference_net_per_component,
+                    "pin_order": i.pin_order,
+                }
+            )
+        return data
 
     def __init__(self, pedb, data, path_lib=None):
         self._pedb = pedb
-        if self._pedb.grpc:
-            self.api = self.Grpc(self)
-        else:
-            self.api = self.DotNet(self)
         self.path_libraries = path_lib
         self.s_parameters_models = [CfgSParameterModel(**i) for i in data]
-
-    def apply(self):
-        self.api.apply()
-
-    def get_data_from_db(self, cfg_components):
-        return self.api.get_data_from_db(cfg_components)
