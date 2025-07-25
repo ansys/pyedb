8875b79c65091b26582d7d52c445d2c207f74ba7
gkorompi <george.korompilis@ansys.com>
2025-07-25 09:22:18 +0300

    FEAT: vlctech

--- src/pyedb/grpc/edb.py
+++ src/pyedb/grpc/edb.py
@@ -897,7 +897,7 @@ class Edb(EdbInit):
         """
         if not working_dir:
             working_dir = os.path.dirname(vlctech_file)
-        command = os.path.join(self.base_path, r"helic\tools\raptorh\bin\make-edb")
+        command = os.path.join(self.base_path, "helic", "tools", "raptorh", "bin", "make-edb")
         if is_windows:
             command += ".exe"
         cmd_make_edb = [
