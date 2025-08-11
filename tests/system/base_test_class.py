a7b2cf10a81089bc386563e7d567053424cd5222
hui-zhou-a <huizhou@synopsys.com>
2025-08-11 13:02:19 +0200

    fix

--- tests/system/base_test_class.py
+++ tests/system/base_test_class.py
@@ -19,12 +19,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import os
-from pathlib import Path
 
 import pytest
-
-from tests.conftest import local_path, test_subfolder
 
 pytestmark = [pytest.mark.unit, pytest.mark.legacy]
 
