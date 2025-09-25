6130034695f77f040fa33114535d6bad31e1aa90
svandenb-dev <simon.vandenbrouck@ansys.com>
2025-09-25 17:03:47 +0200

    fix pyaedt test failing

--- src/pyedb/dotnet/database/general.py
+++ src/pyedb/dotnet/database/general.py
@@ -51,8 +51,9 @@ def convert_netdict_to_pydict(dict_in):
 
     """
     pydict = {}
-    for key in dict_in.Keys:
-        pydict[key] = dict_in[key]
+    if dict_in:
+        for key in dict_in.Keys:
+            pydict[key] = dict_in[key]
     return pydict
 
 
