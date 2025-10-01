86bedbeb1a4679aa827fcd979b543273d86c788e
svandenb-dev <simon.vandenbrouck@ansys.com>
2025-10-01 14:45:39 +0200

    config file 1.0 fix

--- tests/system/test_edb_configuration_1p0.py
+++ tests/system/test_edb_configuration_1p0.py
@@ -271,7 +271,7 @@ class TestClass(BaseTestClass):
         edbapp.build_simulation_project(simconfig)
         assert round(edbapp.components["X1"].solder_ball_height, 6) == 0.00025
         assert round(edbapp.components["U1"].solder_ball_height, 6) == 0.00035
-        edbapp.close_edb()
+        edbapp.close()
 
     def test_build_siwave_project_from_config_file(self):
         """Build Siwave simulation project from configuration file."""
