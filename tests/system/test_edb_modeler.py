6e811465af6e0f79cce0048551f422a97049d86b
hui-zhou-a <hui.zhou@ansys.com>
2025-11-25 13:50:13 +0100

    fix

--- tests/system/test_edb_modeler.py
+++ tests/system/test_edb_modeler.py
@@ -614,7 +614,6 @@ class TestClass(BaseTestClass):
         edbapp = edb_examples.get_si_verse()
         edb2_path = edb_examples.get_package(edbapp=False)
         edbapp.copy_cell_from_edb(edb2_path)
-        edb2_path.close(terminate_rpc_session=False)
         cell_inst = edbapp.modeler.insert_layout_instance_on_layer("analysis", "1_Top", 2, "180deg", "1mm", "2mm", True)
         assert cell_inst.transform.rotation.value == pytest.approx(3.14159265358979)
         assert cell_inst.transform.scale.value == pytest.approx(2)
@@ -628,8 +627,6 @@ class TestClass(BaseTestClass):
         edbapp = edb_examples.get_si_verse()
         edb2_path = edb_examples.get_package(edbapp=False)
         edbapp.copy_cell_from_edb(edb2_path)
-        edb2_path.close(terminate_rpc_session=False)
-
         cell_inst = edbapp.modeler.insert_layout_instance_placement_3d("analysis", rotation_x="180deg", z="-0.33mm")
         assert not cell_inst.is_null
         edbapp.close(terminate_rpc_session=False)
