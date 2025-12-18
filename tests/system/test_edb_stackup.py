d59a41313658262f76ba76023fdade45c5545d25
svandenb-dev <simon.vandenbrouck@ansys.com>
2025-12-18 17:33:16 +0100

    pass #40

--- tests/system/test_edb_stackup.py
+++ tests/system/test_edb_stackup.py
@@ -412,6 +412,8 @@ class TestClass(BaseTestClass):
                     # dotnet is returning 0 elevation on dielectric layer which is wrong.
                     assert data["layers"]["DE2"][parameter] == 0.0
                 else:
+                    if value == "":
+                        value = None
                     assert data["layers"]["DE2"][parameter] == value
         edbapp.close(terminate_rpc_session=False)
 
