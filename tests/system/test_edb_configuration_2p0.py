92ded5c2a4037cead3628ec3f0fefd55c43a3d77
hui-zhou-a <hui.zhou@ansys.com>
2026-02-02 11:01:21 +0100

    FIX

--- tests/system/test_edb_configuration_2p0.py
+++ tests/system/test_edb_configuration_2p0.py
@@ -1394,6 +1394,7 @@ class TestClassSetups(BaseTestClass):
                     "max_num_passes": 10,
                     "max_mag_delta_s": 0.02,
                     "auto_mesh_operation": {
+                        "enabled": True,
                         "trace_ratio_seeding": 3,
                         "signal_via_side_number": 12,
                         "power_ground_via_side_number": 6,
