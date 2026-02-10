4425c4857dfb22aa253f2d60c7dd80bfb8690840
hui-zhou-a <hui.zhou@ansys.com>
2026-02-10 18:21:53 +0100

    draft

--- tests/conftest.py
+++ tests/conftest.py
@@ -112,26 +112,6 @@ class EdbExamples:
         self.example_models_path = example_models_path
         self.test_folder = ""
 
-    def get_local_file_folder(self, name):
-        return os.path.join(self.local_scratch.path, name)
-
-    def _create_test_folder(self, name=None):
-        """Create a local folder under `local_scratch`."""
-        if name:
-            temp = Path(self.local_scratch.path) / name
-            temp.mkdir(parents=True, exist_ok=True)
-            self.test_folder = str(temp)
-        else:
-            self.test_folder = os.path.join(self.local_scratch.path, generate_random_string(6))
-        return self.test_folder
-
-    def _copy_file_folder_into_local_folder(self, file_folder_path):
-        src = os.path.join(self.example_models_path, file_folder_path)
-        local_folder = self._create_test_folder()
-        file_folder_name = os.path.join(local_folder, os.path.split(src)[-1])
-        dst = self.local_scratch.copyfolder(src, file_folder_name)
-        return dst
-
     def copy_test_files_into_local_folder(self, file_folder_path):
         """Copy files or folders from example_models into local test folder."""
         source_folder = Path(__file__).parent / "example_models"
@@ -170,68 +150,72 @@ class EdbExamples:
         else:
             return aedb
 
-    def get_si_verse(self, edbapp=True, additional_files_folders="", version=None):
-        return self._get_test_board(
-            edbapp, additional_files_folders, version, source_file_path="si_verse/ANSYS-HSD_V1.aedb"
-        )
+    def get_si_verse(self, edbapp=True, version=None):
+        target_file = self.copy_test_files_into_local_folder("si_verse/ANSYS-HSD_V1.aedb")[0]
+        if edbapp:
+            version = desktop_version if version is None else version
+            return Edb(target_file, version=version, grpc=self.grpc)
+        else:
+            return target_file
 
-    def get_wirebond_jedec4_project(self, edbapp=True, additional_files_folders="", version=None):
-        return self._get_test_board(
-            edbapp, additional_files_folders, version, source_file_path="wirebond_projects/test_wb_jedec4.aedb"
-        )
+    def get_wirebond_jedec4_project(self, edbapp=True, version=None):
+        target_file = self.copy_test_files_into_local_folder("wirebond_projects/ANSYS-test_wb_jedec4.aedb")[0]
+        if edbapp:
+            version = desktop_version if version is None else version
+            return Edb(target_file, version=version, grpc=self.grpc)
+        else:
+            return target_file
 
     def get_si_verse_sfp(self, edbapp=True, additional_files_folders="", version=None):
-        return self._get_test_board(
-            edbapp, additional_files_folders, version, source_file_path="si_verse/ANSYS_SVP_V1_1_SFP.aedb"
-        )
+        target_file = self.copy_test_files_into_local_folder("si_verse/ANSYS-ANSYS_SVP_V1_1_SFP.aedb")[0]
+        if edbapp:
+            version = desktop_version if version is None else version
+            return Edb(target_file, version=version, grpc=self.grpc)
+        else:
+            return target_file
 
     def get_package(self, edbapp=True, additional_files_folders="", version=None):
         """ "Copy package board file into local folder. A new temporary folder will be created."""
-        return self._get_test_board(
-            edbapp, additional_files_folders, version, source_file_path="TEDB/example_package.aedb"
-        )
+        target_file = self.copy_test_files_into_local_folder("TEDB/ANSYS-example_package.aedb")[0]
+        if edbapp:
+            version = desktop_version if version is None else version
+            return Edb(target_file, version=version, grpc=self.grpc)
+        else:
+            return target_file
 
     def create_empty_edb(self):
-        local_folder = self._create_test_folder()
-        aedb = os.path.join(local_folder, "new_layout.aedb")
-        return Edb(aedb, edbversion=desktop_version, grpc=self.grpc)
+        aedb = os.path.join(self.test_folder, "new_layout.aedb")
+        return Edb(aedb, version=desktop_version, grpc=self.grpc)
 
-    def get_multizone_pcb(self):
-        aedb = self._copy_file_folder_into_local_folder("multi_zone_project.aedb")
-        return Edb(aedb, edbversion=desktop_version, grpc=self.grpc)
+    def get_multizone_pcb(self, version=None):
+        target_file = self.copy_test_files_into_local_folder("multi_zone_project.aedb")[0]
+        version = desktop_version if version is None else version
+        return Edb(target_file, version=version, grpc=self.grpc)
 
-    def get_unit_cell(self):
-        aedb = self._copy_file_folder_into_local_folder("TEDB/unitcell.aedb")
-        return Edb(aedb, edbversion=desktop_version, grpc=self.grpc)
+    def get_unit_cell(self,version=None):
+        target_file = self.copy_test_files_into_local_folder("TEDB/unitcell.aedb")[0]
+        version = desktop_version if version is None else version
+        return Edb(target_file, version=version, grpc=self.grpc)
 
-    def get_no_ref_pins_component(self):
-        aedb = self._copy_file_folder_into_local_folder("TEDB/component_no_ref_pins.aedb")
-        return Edb(aedb, edbversion=desktop_version, grpc=self.grpc)
+    def get_no_ref_pins_component(self, version=None):
+        target_file = self.copy_test_files_into_local_folder("TEDB/component_no_ref_pins.aedb")[0]
+        version = desktop_version if version is None else version
+        return Edb(target_file, version=version, grpc=self.grpc)
 
     def get_si_board(self, edbapp=True, additional_files_folders="", version=None):
-        return self._get_test_board(
-            edbapp,
-            additional_files_folders,
-            version,
-            source_file_path="si_board/si_board.aedb",
-        )
-
-    def load_edb(self, edb_path, copy_to_temp=True, **kwargs):
-        if copy_to_temp:
-            aedb = self._copy_file_folder_into_local_folder(edb_path)
+        target_file = self.copy_test_files_into_local_folder("si_board/si_board.aedb")[0]
+        if edbapp:
+            version = desktop_version if version is None else version
+            return Edb(target_file, version=version, grpc=self.grpc)
         else:
-            aedb = edb_path
-        return Edb(edbpath=aedb, edbversion=desktop_version, grpc=self.grpc, **kwargs)
+            return target_file
+
+    def load_edb(self, edb_path, **kwargs):
+        return Edb(edbpath=edb_path, edbversion=desktop_version, grpc=self.grpc, **kwargs)
 
     def load_dxf_edb(self):
-        aedb = self._copy_file_folder_into_local_folder("dxf_swap/starting_edb/starting_edb.aedb")
+        aedb = self.copy_test_files_into_local_folder("dxf_swap/starting_edb/starting_edb.aedb")[0]
         return Edb(edbpath=aedb, version=desktop_version, grpc=True)
-
-    def copy_project_for_job_manager(self, local_scratch):
-        example_project = os.path.join(example_models_path, "test_project_for_job_manager.aedb")
-        target_path = os.path.join(local_scratch.path, "project.aedb")
-        local_scratch.copyfolder(example_project, target_path)
-        return target_path
 
     def get_log_file_example(self):
         return os.path.join(self.example_models_path, "test.log")
