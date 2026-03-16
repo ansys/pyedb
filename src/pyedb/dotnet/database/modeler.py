3501f6d7a879b6afaee2397d23e8be2b930dd841
svandenb-dev <simon.vandenbrouck@ansys.com>
2026-03-16 07:31:34 +0100

    Primitive queries refactored

--- src/pyedb/dotnet/database/modeler.py
+++ src/pyedb/dotnet/database/modeler.py
@@ -33,7 +33,7 @@ if TYPE_CHECKING:
 from pyedb.dotnet.clr_module import Tuple
 from pyedb.dotnet.database.cell.primitive.bondwire import Bondwire
 from pyedb.dotnet.database.dotnet.primitive import CircleDotNet, PathDotNet, RectangleDotNet
-from pyedb.dotnet.database.edb_data.primitives_data import EdbPolygon, Primitive, cast
+from pyedb.dotnet.database.edb_data.primitives_data import EdbPolygon, cast
 from pyedb.dotnet.database.edb_data.utilities import EDBStatistics
 from pyedb.dotnet.database.general import convert_py_list_to_net_list
 from pyedb.misc.decorators import deprecate_argument_name, deprecated, deprecated_property
