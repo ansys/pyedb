9a0382e967b5e9aa22422a14a2ba2fc2fd198410
svandenb-dev <simon.vandenbrouck@ansys.com>
2026-03-16 16:45:29 +0100

    grpc net structure 3D added

--- src/pyedb/grpc/database/hierarchy/structure_3d.py
+++ src/pyedb/grpc/database/hierarchy/structure_3d.py
@@ -20,100 +20,105 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from pyedb.dotnet.database.edb_data.nets_data import NetDotNet
+from ansys.edb.core.hierarchy.structure3d import Structure3D as CoreStructure3D
 
 
 class Structure3D:
+    """Class for storing structure 3D components."""
+
     def __init__(self, pedb, core):
         self._pedb = pedb
         self.core = core
 
     @classmethod
     def create(cls, edb, name: str) -> "Structure3D":
-        """Create a new structure 3D object
+        """Create structure 3D component.
+
+        Parameters
+        ----------
+        edb : EDB
+            The EDB instance the structure 3D component belongs to.
+        name : str
+            The name of the structure 3D component.
 
         Returns
         -------
         Structure3D
-            The created structure 3D object.
+            The created structure 3D component.
         """
-        core = edb._edb.Cell.Hierarchy.Structure3D.Create(edb.layout.core, name)
+        core = CoreStructure3D.create(edb.layout.core, name)
         return Structure3D(pedb=edb, core=core)
 
     def delete(self):
-        """ "Delete the structure 3D object"""
-        self.core.Delete()
+        """Delete structure 3D component."""
+        self.core.delete()
 
     @property
     def id(self) -> int:
-        """The id of the structure 3D object
+        """Return structure 3D component.
 
         Returns
         -------
         int
-            The id of the structure 3D object.
+            The structure 3D component unique ID.
         """
-        return self.core.GetId()
+        return self.core.edb_uid
 
     @property
     def location(self) -> tuple[float, float]:
-        """The location of the structure 3D object.
+        """ "Return structure 3D component.
 
         Returns
         -------
         tuple[float, float]
-            The location of the structure 3D object.
+            The structure 3D component location as a tuple of x and y coordinates.
         """
-        location = self.core.GetLocation()
-        return location[1], location[2]
+        return tuple([self._pedb._value_setter(val) for val in self.core.location])
 
     @location.setter
     def location(self, location: tuple[float, float]):
-        if isinstance(location, tuple) or isinstance(location, list):
-            self.core.SetLocation(location[0], location[1])
+        self.core.location = tuple(location)
 
     @property
     def name(self) -> str:
-        """The name of the structure 3D object.
+        """ "Return structure 3D component.
 
         Returns
         -------
         str
-            The name of the structure 3D object.
+            The structure 3D component name.
         """
-        return self.core.GetName()
+        return self.core.name
 
     @name.setter
     def name(self, name: str):
-        self.core.SetName(name)
+        self.core.name = name
 
     @property
     def material(self) -> str:
-        """The material name of the structure 3D object.
+        """ "Return structure 3D component.
 
-        Returns
-        -------
         str
-            The material name of the structure 3D object.
+            The structure 3D component material.
         """
-        return self.core.GetMaterial()
+        return self.core.material
 
     @material.setter
     def material(self, material: str):
-        self.core.SetMaterial(material)
+        self.core.material = material
 
     @property
     def net(self) -> str:
-        """The net of the structure 3D object.
+        """ "Return structure 3D component net name
 
         Returns
         -------
         str
-            The net of the structure 3D object.
+            The structure 3D component net name.
         """
-        return self.core.GetNet().GetName()
+        return self.core.net.name
 
     @net.setter
     def net(self, net: str):
         net = self._pedb.nets.find_or_create_net(net)
-        self.core.SetNet(net.api_object)
+        self.core.net = net.core
