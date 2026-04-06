6ba89263bc356dde9ba79797dd29043b3ed29df5
hui-zhou-a <huizhou@synopsys.com>
2026-04-06 09:56:05 +0200

    fix

--- tests/system/test_edb_configuration_2p0.py
+++ tests/system/test_edb_configuration_2p0.py
@@ -1118,7 +1118,6 @@ class TestClassSetups(BaseTestClass):
                     "freq_sweep": [],
                     "auto_mesh_operation": {
                         "enabled": False,
-                        "power_ground_via_side_number": 6,
                         "signal_via_side_number": 12,
                         "trace_ratio_seeding": 3,
                     },
@@ -1204,7 +1203,6 @@ class TestClassSetups(BaseTestClass):
                         "enabled": True,
                         "trace_ratio_seeding": 3,
                         "signal_via_side_number": 12,
-                        "power_ground_via_side_number": 6,
                     },
                 },
             ],
