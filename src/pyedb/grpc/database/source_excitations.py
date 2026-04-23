5ff6bf86670194b8852e768e093aae81ca364d11
svandenb-dev <simon.vandenbrouck@ansys.com>
2026-04-23 13:06:44 +0200

    edge terminal fix

--- src/pyedb/grpc/database/source_excitations.py
+++ src/pyedb/grpc/database/source_excitations.py
@@ -260,7 +260,7 @@ class SourceExcitationInternal:
         if isinstance(point_on_edge, tuple):
             point_on_edge = CorePointData(point_on_edge)
         elif isinstance(point_on_edge, list):
-            point_on_edge = [CorePointData(point) for point in point_on_edge]
+            point_on_edge = [CorePointData(self._pedb.value(point)) for point in point_on_edge]
         primitive_lookup_id = prim_id.edb_uid if isinstance(prim_id, Primitive) else prim_id
         try:
             prim = self._pedb.layout.find_object_by_id(primitive_lookup_id)
@@ -271,7 +271,7 @@ class SourceExcitationInternal:
             pos_edge = [CorePrimitiveEdge.create(prim.core, pt) for pt in point_on_edge]
         else:
             pos_edge = [CorePrimitiveEdge.create(prim.core, point_on_edge)]
-        return EdgeTerminal.create(layout=prim.layout, name=terminal_name, edge=pos_edge, net=prim.net, is_ref=is_ref)
+        return EdgeTerminal.create(layout=prim.layout, name=terminal_name, edges=pos_edge, net=prim.net, is_ref=is_ref)
 
 
 class SourceExcitation(SourceExcitationInternal):
