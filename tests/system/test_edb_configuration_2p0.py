47c30b9007690118795b8ba39257ca5488b1424b
svandenb-dev <simon.vandenbrouck@ansys.com>
2026-05-03 09:59:05 +0200

    test dotnet skipped

--- tests/system/test_edb_configuration_2p0.py
+++ tests/system/test_edb_configuration_2p0.py
@@ -944,6 +944,7 @@ class TestClassTerminals(BaseTestClass):
         "name": "bundle_terminal",
     }
 
+    pytest.mark.skipif(not config["use_grpc"], reason="DotNet bug always returning Wave port.")
     def test_padstack_instance_terminal(self):
         edbapp = self.edb_examples.get_si_verse()
         edbapp.configuration.load({"terminals": [self.terminal1]}, append=False)
