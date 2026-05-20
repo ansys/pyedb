ccf5829369383253a72e76f7e8677fd49a9fcac8
svandenb-dev <simon.vandenbrouck@ansys.com>
2026-05-20 13:23:12 +0200

    test merged

--- tests/unit/test_cfg_api.py
+++ tests/unit/test_cfg_api.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from unittest.mock import MagicMock
+
 from pydantic import ValidationError
 import pytest
 
