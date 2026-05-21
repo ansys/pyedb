112b98b9c1785c7dd0411a7875131d1a29b055c6
svandenb-dev <74993647+svandenb-dev@users.noreply.github.com>
2026-05-21 05:33:23 +0200

    REFACTOR: cfg_package_definition refactoring (#2140)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/pyedb/configuration/cfg_package_definition.py
+++ src/pyedb/configuration/cfg_package_definition.py
@@ -20,63 +20,141 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from pyedb.configuration.cfg_common import CfgBase
+"""Build thermal package-definition configuration entries."""
+
+from typing import Any, List, Optional
+
+from pydantic import Field
+
+from pyedb.configuration.cfg_common import CfgBaseModel
 from pyedb.generic.settings import settings
 
 
-class CfgPackage(CfgBase):
-    """Configuration package class."""
-
-    # Attributes cannot be set to package definition class or don't exist in package definition class.
-    protected_attributes = ["name", "apply_to_all", "components", "extent_bounding_box", "component_definition"]
-
-    def __init__(self, **kwargs):
-        self.name = kwargs.get("name", None)
-        self.component_definition = kwargs.get("component_definition", None)
-        self.maximum_power = kwargs.get("maximum_power", None)
-        self.thermal_conductivity = kwargs.get("thermal_conductivity", None)
-        self.theta_jb = kwargs.get("theta_jb", None)
-        self.theta_jc = kwargs.get("theta_jc", None)
-        self.height = kwargs.get("height", None)
-        self.apply_to_all = kwargs.get("apply_to_all", None)
-        self.components = kwargs.get("components", [])
-        self.extent_bounding_box = kwargs.get("extent_bounding_box", None)
-        self._heatsink = CfgHeatSink(**kwargs["heatsink"]) if "heatsink" in kwargs else None
-
-    @property
-    def heatsink(self):
-        return self._heatsink
-
-    @heatsink.setter
-    def heatsink(self, value):
-        self._heatsink = value
-
-
-class CfgHeatSink(CfgBase):
+class CfgHeatSink(CfgBaseModel):
     """Configuration heat sink class."""
 
-    def __init__(self, **kwargs):
-        self.fin_base_height = kwargs.get("fin_base_height", None)
-        self.fin_height = kwargs.get("fin_height", None)
-        self.fin_orientation = kwargs.get("fin_orientation", None)
-        self.fin_spacing = kwargs.get("fin_spacing", None)
-        self.fin_thickness = kwargs.get("fin_thickness", None)
+    model_config = {"populate_by_name": True, "extra": "ignore"}
+
+    fin_base_height: Optional[Any] = None
+    fin_height: Optional[Any] = None
+    fin_orientation: Optional[str] = None
+    fin_spacing: Optional[Any] = None
+    fin_thickness: Optional[Any] = None
+
+    def get_attributes(self, exclude=None):
+        """Return non-null attribute dict (CfgBase compatibility)."""
+        return self.model_dump(exclude_none=True)
+
+
+class CfgPackage(CfgBaseModel):
+    """Configuration package class."""
+
+    model_config = {"populate_by_name": True, "extra": "ignore", "arbitrary_types_allowed": True}
+
+    # Attributes cannot be set to package definition class or don't exist in package definition class.
+    _protected_attributes: List[str] = [
+        "name",
+        "apply_to_all",
+        "components",
+        "extent_bounding_box",
+        "component_definition",
+    ]
+
+    name: Optional[str] = None
+    component_definition: Optional[str] = None
+    apply_to_all: Optional[bool] = None
+    components: List[str] = Field(default_factory=list)
+    maximum_power: Optional[Any] = None
+    thermal_conductivity: Optional[Any] = None
+    theta_jb: Optional[Any] = None
+    theta_jc: Optional[Any] = None
+    height: Optional[Any] = None
+    extent_bounding_box: Optional[Any] = None
+    heatsink: Optional[CfgHeatSink] = None
+
+    def get_attributes(self, exclude=None):
+        """Return dict of non-null/non-protected attributes (CfgBase compatibility)."""
+        protected = set(self._protected_attributes) | {"heatsink"}
+        if exclude:
+            protected.update(exclude if isinstance(exclude, list) else [exclude])
+        return {k: v for k, v in self.model_dump(exclude_none=True).items() if k not in protected}
+
+    def set_attributes(self, pedb_object):
+        """Set non-protected attributes onto *pedb_object* (CfgBase compatibility)."""
+        attrs = self.get_attributes()
+        for attr, value in attrs.items():
+            if attr not in dir(pedb_object):
+                raise AttributeError(f"Invalid attribute '{attr}' in '{pedb_object}'")
+            setattr(pedb_object, attr, value)
+
+    def set_heatsink(
+        self,
+        fin_base_height=None,
+        fin_height=None,
+        fin_orientation=None,
+        fin_spacing=None,
+        fin_thickness=None,
+    ):
+        """Attach heat-sink fin geometry to this package definition.
+
+        Parameters
+        ----------
+        fin_base_height : str, optional
+            Base height of the fin array, e.g. ``"0.5mm"``.
+        fin_height : str, optional
+            Fin height, e.g. ``"3mm"``.
+        fin_orientation : str, optional
+            Fin orientation: ``"x_oriented"`` or ``"y_oriented"``.
+        fin_spacing : str, optional
+            Centre-to-centre fin spacing, e.g. ``"1mm"``.
+        fin_thickness : str, optional
+            Fin thickness, e.g. ``"0.2mm"``.
+
+        Returns
+        -------
+        CfgHeatSink
+            The newly created heat-sink object.
+
+        Examples
+        --------
+        >>> pkg.set_heatsink(
+        ...     fin_base_height="0.5mm",
+        ...     fin_height="3mm",
+        ...     fin_orientation="x_oriented",
+        ...     fin_spacing="1mm",
+        ...     fin_thickness="0.2mm",
+        ... )
+        """
+        hs = CfgHeatSink(
+            fin_base_height=fin_base_height,
+            fin_height=fin_height,
+            fin_orientation=fin_orientation,
+            fin_spacing=fin_spacing,
+            fin_thickness=fin_thickness,
+        )
+        object.__setattr__(self, "heatsink", hs)
+        return hs
 
 
 class CfgPackageDefinitions:
-    def get_parameter_from_edb(self):
+    """Manage thermal package definitions for the ``package_definitions`` section."""
+
+    def get_parameters_from_edb(self):
+        """Read thermal package definitions from EDB."""
         package_definitions = []
+        _pkg_field_names = set(CfgPackage.model_fields.keys())
+        _hs_field_names = set(CfgHeatSink.model_fields.keys())
         for pkg_name, pkg_obj in self._pedb.definitions.package.items():
             pkg = {}
             pkg_attrs = [i for i in dir(pkg_obj) if not i.startswith("_")]
-            pkg_attrs = {i for i in pkg_attrs if i in CfgPackage().__dict__}
+            pkg_attrs = {i for i in pkg_attrs if i in _pkg_field_names}
             for pkg_attr_name in pkg_attrs:
                 pkg[pkg_attr_name] = getattr(pkg_obj, pkg_attr_name)
             hs_obj = pkg_obj.heat_sink
             if hs_obj:
                 hs = {}
                 hs_attrs = [i for i in dir(hs_obj) if not i.startswith("_")]
-                hs_attrs = [i for i in hs_attrs if i in CfgHeatSink().__dict__]
+                hs_attrs = [i for i in hs_attrs if i in _hs_field_names]
                 for hs_attr_name in hs_attrs:
                     hs[hs_attr_name] = getattr(hs_obj, hs_attr_name)
                 pkg["heatsink"] = hs
@@ -84,7 +162,8 @@ class CfgPackageDefinitions:
 
         return package_definitions
 
-    def set_parameter_to_edb(self):
+    def set_parameters_to_edb(self):
+        """Write all configured package definitions into the open EDB design."""
         if settings.is_grpc:
             from pyedb.grpc.database.definition.package_def import PackageDef
         else:
@@ -102,8 +181,7 @@ class CfgPackageDefinitions:
             pkg.set_attributes(package_def)
 
             if pkg.heatsink:
-                attrs = pkg.heatsink.get_attributes()
-                package_def.set_heatsink(**attrs)
+                package_def.set_heatsink(**pkg.heatsink.get_attributes())
 
             comp_list = dict()
             if pkg.apply_to_all:
@@ -121,12 +199,90 @@ class CfgPackageDefinitions:
             for _, i in comp_list.items():
                 i.package_def = pkg.name
 
-    def __init__(self, pedb, data):
+    get_parameter_from_edb = get_parameters_from_edb
+    set_parameter_to_edb = set_parameters_to_edb
+
+    def __init__(self, pedb=None, data=None):
         self._pedb = pedb
-        self.packages = [CfgPackage(**package) for package in data]
+        self.packages = [CfgPackage(**package) for package in (data or [])]
+
+    def add(
+        self,
+        name: str,
+        component_definition: str,
+        apply_to_all=None,
+        components=None,
+        maximum_power=None,
+        thermal_conductivity=None,
+        theta_jb=None,
+        theta_jc=None,
+        height=None,
+        extent_bounding_box=None,
+    ):
+        """Add a thermal package definition entry.
+
+        Parameters
+        ----------
+        name : str
+            Package definition name, e.g. ``"PKG_U1"``.
+        component_definition : str
+            Component part-definition name.
+        apply_to_all : bool, optional
+            Assign the package to every matching component when ``True``.
+        components : list of str, optional
+            Specific reference designators to target when *apply_to_all* is
+            ``False``.
+        maximum_power : str, optional
+            Maximum power dissipation, e.g. ``"5W"``.
+        thermal_conductivity : str, optional
+            Package thermal conductivity.
+        theta_jb : str, optional
+            Junction-to-board thermal resistance, e.g. ``"10C/W"``.
+        theta_jc : str, optional
+            Junction-to-case thermal resistance, e.g. ``"5C/W"``.
+        height : str, optional
+            Package height, e.g. ``"1mm"``.
+        extent_bounding_box : dict, optional
+            Custom bounding-box extent override.
+
+        Returns
+        -------
+        CfgPackage
+            The newly created package object.  Call
+            :meth:`CfgPackage.set_heatsink` on it to add fin geometry.
+
+        Examples
+        --------
+        >>> pkg = cfg.package_definitions.add(
+        ...     "PKG_U1",
+        ...     component_definition="IC_U1",
+        ...     apply_to_all=True,
+        ...     maximum_power="5W",
+        ...     theta_jb="10C/W",
+        ...     theta_jc="5C/W",
+        ...     height="1mm",
+        ... )
+        >>> pkg.set_heatsink(fin_base_height="0.5mm", fin_height="3mm")
+        """
+        pkg = CfgPackage(
+            name=name,
+            component_definition=component_definition,
+            apply_to_all=apply_to_all,
+            components=components or [],
+            maximum_power=maximum_power,
+            thermal_conductivity=thermal_conductivity,
+            theta_jb=theta_jb,
+            theta_jc=theta_jc,
+            height=height,
+            extent_bounding_box=extent_bounding_box,
+        )
+        self.packages.append(pkg)
+        return pkg
 
     def apply(self):
-        self.set_parameter_to_edb()
+        """Write all configured package definitions into the open EDB design."""
+        self.set_parameters_to_edb()
 
     def get_data_from_db(self):
-        return self.get_parameter_from_edb()
+        """Read package definitions from EDB."""
+        return self.get_parameters_from_edb()
